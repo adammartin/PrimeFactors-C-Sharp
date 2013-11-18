d2d09e52fcfdd5a5e6cff86e35515207572aa938
Adam Martin <yance.martin@gmail.com>
2013-11-18 09:23:04 -0600

    Initial commit of xunit

---
+++ Factors/PrimeFactors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factors
+{
+    public class PrimeFactors
+    {
+        public static List<int> Primes(int p)
+        {
+            return new List<int>();
+        }
+    }
+}
---
+++ PrimeFactorsTest/PrimeFactorsTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Xunit;
+using Factors;
+using System.Collections.Generic;
+
+namespace PrimeFactorsTest
+{
+    public class PrimeFactorsTest
+    {
+        [Fact]
+        public void TestOne()
+        {
+            Assert.Equal(0, PrimeFactors.Primes(1).Count);
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            Assert.Equal(new List<int>{2}, PrimeFactors.Primes(2));
+        }
+
+    }
+}
